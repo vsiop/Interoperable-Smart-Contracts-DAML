dce99c2b6c6fbf3b5d818c3bcdfd49cb1987170c
Vasilis Siopidis <51744667+vsiop@users.noreply.github.com>
2021-07-12 14:00:00 +0300

    Add files via upload

---
+++ DAML_Oracles/printActiveSawtooth.py
@@ -0,0 +1,23 @@
+import json
+import os
+import dazl
+from configparser import ConfigParser
+
+parser = ConfigParser()
+parser.read('configvaluesSawtooth.ini')
+network = parser.get('url_value','url')
+dataTemplate = parser.get('data_template','template_data')
+imagesTemplate = parser.get('images_template','template_images')
+observer = parser.get('DAML_observer','observer')
+
+def printActiveDataSawtooth():
+    with dazl.simple_client(network, observer) as client: 
+      client.ready()
+      contract_dict = client.find(dataTemplate)
+      for contract in contract_dict:
+          print(contract)
+          print(type(contract))
+        
+
+if __name__ == '__main__':
+ printActiveDataSawtooth()
---
+++ DAML_Oracles/queryDataOnDateSawtooth.py
@@ -0,0 +1,26 @@
+import json
+import os
+import dazl
+from datetime import date
+import base64
+import sys
+from configparser import ConfigParser
+
+parser = ConfigParser()
+parser.read('configvaluesSawtooth.ini')
+network = parser.get('url_value','url')
+dataTemplate = parser.get('data_template','template_data')
+imagesTemplate = parser.get('images_template','template_images')
+observer = parser.get('DAML_observer','observer')
+
+
+
+def printDataOnDateSawtooth():
+    with dazl.simple_client(network, observer) as client:
+      client.ready()
+      today = date.today()
+      contract_dict = client.find(dataTemplate, {"weatherData": {"date": today }})
+      print(contract_dict)
+
+if __name__ == '__main__':
+ printDataOnDateSawtooth()
